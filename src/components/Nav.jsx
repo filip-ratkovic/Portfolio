6fa87ef7958e9db76d3994b4311fbc778ea89f93
Filip <filip.ratkovic.fr@gmail.com>
2022-08-14 17:01:53 +0200

    Navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import {React, useEffect, useState} from 'react';
 import { Routes, Route, Link, NavLink } from 'react-router-dom';
 
+
 function Nav() {
+
+// const [isActive, setIsActive] = useState(false);
+useEffect(() => {
+    const test = document.querySelectorAll(".test");
+console.log(test);
+
+test.forEach((el) => {
+    el.addEventListener("click",()=>{
+
+removeActive();
+        el.classList.toggle("active")
+
+    })
+})
+
+const  removeActive = (el) => {
+    test.forEach((el) => {
+        el.classList.remove("active")
+    })
+    }
+})
+
+
 
 
 
@@ -10,13 +34,13 @@ function Nav() {
     return (
         <nav className="nav">
             <div className="navName">
-                <Link id="navLink" to={"/"}>
-                    <h3>Filip Ratković</h3></Link>
+                <Link  id="navLink" to={"/"}>
+                    <h3 className="test">Filip Ratković</h3></Link>
             </div>
             <div className="navList">
-                <Link id="navLink" to="/about"><p >About me</p></Link>
-                <Link id="navLink" to="/project"><p >Projects</p></Link>
-                <Link id="navLink" to="/contact"><p >Contact</p></Link>
+                <Link id="navLink" to="/about"><p className="test" >About me</p></Link>
+                <Link id="navLink" to="/project"><p className="test" >Projects</p></Link>
+                <Link id="navLink" to="/contact"><p className="test" >Contact</p></Link>
             </div>
 
         </nav>
