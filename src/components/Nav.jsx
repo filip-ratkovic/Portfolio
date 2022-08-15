588ba3b39344aadb57535055e111fbcd3d2a01ba
Filip <filip.ratkovic.fr@gmail.com>
2022-08-15 23:35:47 +0200

    Nav onclick change color

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,29 +1,64 @@
-import {React, useEffect, useState} from 'react';
+import { React, useEffect, useState } from 'react';
 import { Routes, Route, Link, NavLink } from 'react-router-dom';
 
 
 function Nav() {
 
-// const [isActive, setIsActive] = useState(false);
-useEffect(() => {
-    const test = document.querySelectorAll(".test");
-console.log(test);
 
-test.forEach((el) => {
-    el.addEventListener("click",()=>{
 
-removeActive();
-        el.classList.toggle("active")
+    // useEffect(() => {
+    //     const test = document.querySelectorAll(".test");
+    // console.log(test);
 
-    })
-})
+    // test.forEach((el) => {
+    //     el.addEventListener("click",()=>{
 
-const  removeActive = (el) => {
-    test.forEach((el) => {
-        el.classList.remove("active")
-    })
-    }
-})
+    // removeActive();
+    //         el.classList.toggle("active")
+
+    //     })
+    // })
+
+    // const  removeActive = (el) => {
+    //     test.forEach((el) => {
+    //         el.classList.remove("active")
+    //     })
+    //     }
+    // })
+
+    const [homeActive, setHomeActive] = useState(false);
+    const [projectActive, setProjectActive] = useState(false);
+    const [aboutActive, setAboutActive] = useState(false);
+    const [contactActive, setContactActive] = useState(false);
+
+
+    const handleHomeClick = event => {
+        setHomeActive(current => !current);
+        setAboutActive(false)
+        setProjectActive(false);
+        setContactActive(false)
+    };
+
+    const handleAboutClick = event => {
+        setAboutActive(current => !current);
+        setHomeActive(false)
+        setProjectActive(false);
+        setContactActive(false)
+    };
+
+    const handleProjectClick = event => {
+        setProjectActive(current => !current);
+        setAboutActive(false)
+        setHomeActive(false);
+        setContactActive(false)
+    };
+
+    const handleContactClick = event => {
+        setContactActive(current => !current);
+        setAboutActive(false)
+        setProjectActive(false);
+        setHomeActive(false)
+    };
 
 
 
@@ -34,13 +69,21 @@ const  removeActive = (el) => {
     return (
         <nav className="nav">
             <div className="navName">
-                <Link  id="navLink" to={"/"}>
-                    <h3 className="test">Filip Ratković</h3></Link>
+                <Link id="navLink" to={"/"}>
+                    <h3 className={` ${homeActive ? "active" : ""}`} onClick={handleHomeClick}
+                    >Filip Ratković</h3></Link>
             </div>
             <div className="navList">
-                <Link id="navLink" to="/about"><p className="test" >About me</p></Link>
-                <Link id="navLink" to="/project"><p className="test" >Projects</p></Link>
-                <Link id="navLink" to="/contact"><p className="test" >Contact</p></Link>
+                <Link id="navLink" to="/about"><p
+                    className={` ${aboutActive ? "active" : ""}`} onClick={handleAboutClick}
+                >About me</p></Link>
+                <Link id="navLink" to="/project"><p
+                    className={` ${projectActive ? "active" : ""}`} onClick={handleProjectClick}
+                >Projects</p></Link>
+                <Link id="navLink" to="/contact"><p
+                    className={` ${contactActive ? "active" : ""}`} onClick={handleContactClick}
+
+                >Contact</p></Link>
             </div>
 
         </nav>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,7 +6,7 @@ import movie from "./photos/movie.png"
 function Projects() {
   return (
     <main className="main">
-        <div className="projectsContainer">
+        {/* <div className="projectsContainer">
             <div className="projectText">
                 <h2>Project</h2>
                 <h1>Cattle</h1>
@@ -22,10 +22,11 @@ function Projects() {
             </div>
         </div>
 
-        <hr />
+        <hr /> */}
                          {/* PROJECT MOVIE DB  */}
 
-        <div className="projectsContainer">
+        {/* <div className="projectsContainer">
+            <div className="projectMobile"></div>
             <div className="projectText">
                 <h2>Project</h2>
                 <h1>Movie DB</h1>
@@ -38,7 +39,32 @@ function Projects() {
             <img src={movie} alt="" />
                 <img id="pngPhoto" src={avengers} alt="Avengers" />
             </div>
+        </div> */}
+
+<div className="projectsContainer">
+            <div className="projectText">
+                <h2>Project</h2>
+                <h1>Movie DB</h1>
+                <h5>Lorem, ipsum dolor.
+                </h5>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus reprehenderit, officiis similique, ab ipsa consequuntur</p>
+                <button><a target="_blank" href="https://filip-ratkovic.github.io/grupa-6/movie/index.html">Explore ↗ </a> </button>
+            </div>
+            <div className="projectPhoto">
+            <div className="projectMobile">
+                
+            </div>
+            <div className="projects" >
+            <img src={movie} alt="" />
+                <img id="pngPhoto" src={avengers} alt="Avengers" />
+            </div>
+            </div>
         </div>
+
+        <hr />
+
+
+
     </main>
   )
 }
