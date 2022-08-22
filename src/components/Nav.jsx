8a361be9ff92ef14987ada6b44834ecc6806df35
Filip <filip.ratkovic.fr@gmail.com>
2022-08-22 14:51:42 +0200

    Projects

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,67 +4,51 @@ import { Routes, Route, Link, NavLink } from 'react-router-dom';
 
 function Nav() {
 
-
-
-    // useEffect(() => {
-    //     const test = document.querySelectorAll(".test");
-    // console.log(test);
-
-    // test.forEach((el) => {
-    //     el.addEventListener("click",()=>{
-
-    // removeActive();
-    //         el.classList.toggle("active")
-
-    //     })
-    // })
-
-    // const  removeActive = (el) => {
-    //     test.forEach((el) => {
-    //         el.classList.remove("active")
-    //     })
-    //     }
-    // })
-
-    const [homeActive, setHomeActive] = useState(false);
+    const [homeActive, setHomeActive] = useState(true);
     const [projectActive, setProjectActive] = useState(false);
     const [aboutActive, setAboutActive] = useState(false);
     const [contactActive, setContactActive] = useState(false);
 
 
     const handleHomeClick = event => {
-        setHomeActive(current => !current);
-        setAboutActive(false)
-        setProjectActive(false);
-        setContactActive(false)
+        if (homeActive) { }
+        else {
+            setHomeActive(current => !current);
+            setAboutActive(false)
+            setProjectActive(false);
+            setContactActive(false)
+        }
     };
 
     const handleAboutClick = event => {
-        setAboutActive(current => !current);
-        setHomeActive(false)
-        setProjectActive(false);
-        setContactActive(false)
+        if (aboutActive) { }
+        else {
+            setAboutActive(current => !current);
+            setHomeActive(false)
+            setProjectActive(false);
+            setContactActive(false)
+        }
     };
 
     const handleProjectClick = event => {
-        setProjectActive(current => !current);
-        setAboutActive(false)
-        setHomeActive(false);
-        setContactActive(false)
+        if (projectActive) { }
+        else {
+            setProjectActive(current => !current);
+            setAboutActive(false)
+            setHomeActive(false);
+            setContactActive(false)
+        }
     };
 
     const handleContactClick = event => {
-        setContactActive(current => !current);
-        setAboutActive(false)
-        setProjectActive(false);
-        setHomeActive(false)
+        if (contactActive) { }
+        else {
+            setContactActive(current => !current);
+            setAboutActive(false)
+            setProjectActive(false);
+            setHomeActive(false)
+        }
     };
-
-
-
-
-
-
 
     return (
         <nav className="nav">
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,72 +1,80 @@
-import React from 'react'
+import { React, useState } from 'react'
 import cattle from "./photos/cattle.png"
 import avengers from "./photos/avengers.png"
 import movie from "./photos/movie.png"
 
 function Projects() {
-  return (
-    <main className="main">
-        {/* <div className="projectsContainer">
-            <div className="projectText">
-                <h2>Project</h2>
-                <h1>Cattle</h1>
-                <h5>Lorem, ipsum dolor.
-                </h5>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus reprehenderit, officiis similique, ab ipsa consequuntur</p>
-                <button><a target="_blank" href="https://filip-ratkovic.github.io/Udemy/cattle/index.html">Explore ↗ </a> </button>
-            </div>
-            <div className="projects" >
-            <img src={cattle} alt="" />
-            <div className="png">
-            </div>
-            </div>
-        </div>
-
-        <hr /> */}
-                         {/* PROJECT MOVIE DB  */}
-
-        {/* <div className="projectsContainer">
-            <div className="projectMobile"></div>
-            <div className="projectText">
-                <h2>Project</h2>
-                <h1>Movie DB</h1>
-                <h5>Lorem, ipsum dolor.
-                </h5>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus reprehenderit, officiis similique, ab ipsa consequuntur</p>
-                <button><a target="_blank" href="https://filip-ratkovic.github.io/grupa-6/movie/index.html">Explore ↗ </a> </button>
-            </div>
-            <div className="projects" >
-            <img src={movie} alt="" />
-                <img id="pngPhoto" src={avengers} alt="Avengers" />
-            </div>
-        </div> */}
-
-<div className="projectsContainer">
-            <div className="projectText">
-                <h2>Project</h2>
-                <h1>Movie DB</h1>
-                <h5>Lorem, ipsum dolor.
-                </h5>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus reprehenderit, officiis similique, ab ipsa consequuntur</p>
-                <button><a target="_blank" href="https://filip-ratkovic.github.io/grupa-6/movie/index.html">Explore ↗ </a> </button>
-            </div>
-            <div className="projectPhoto">
-            <div className="projectMobile">
-                
-            </div>
-            <div className="projects" >
-            <img src={movie} alt="" />
-                <img id="pngPhoto" src={avengers} alt="Avengers" />
-            </div>
-            </div>
-        </div>
-
-        <hr />
+    const [leftActive, setLeftActive] = useState(true);
+    const [rightActive, setRightActive] = useState(false);
 
 
+    const clickHandlerLeft = event => {
+        if (leftActive) {
 
-    </main>
-  )
+        } else {
+            setLeftActive(current => !current);
+            setRightActive(false);
+        }
+    }
+
+    const clickHandlerRight = event => {
+
+        if (rightActive) {
+
+        } else {
+            setRightActive(current => !current);
+            setLeftActive(false);
+        }
+
+    }
+    return (
+        <main className="main">
+            <div className="projectsContainer">
+                <div className="projectText">
+                    <h2>Project</h2>
+                    <h1>Movie DB</h1>
+                    <h5>Lorem, ipsum dolor.
+                    </h5>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus reprehenderit, officiis similique, ab ipsa consequuntur</p>
+                    <button><a target="_blank" href="https://filip-ratkovic.github.io/grupa-6/movie/index.html">Explore ↗ </a> </button>
+                </div>
+                <div className="projectPhoto">
+                    <div className={`projects ${leftActive ? 'active' : ''}`}
+                        onClick={clickHandlerLeft} style={{ backgroundImage: `url(${movie})` }}>
+                    </div>
+                    <div className={`projects ${rightActive ? 'active' : ''}`}
+                        onClick={clickHandlerRight}>
+                    </div>
+                </div>
+            </div>
+
+            <hr />
+
+            <div className="projectsContainer">
+                <div className="projectText">
+                    <h2>Project</h2>
+                    <h1>Movie DB</h1>
+                    <h5>Lorem, ipsum dolor.
+                    </h5>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus reprehenderit, officiis similique, ab ipsa consequuntur</p>
+                    <button><a target="_blank" href="https://filip-ratkovic.github.io/Udemy/cattle/index.html">Explore ↗ </a> </button>
+                </div>
+                <div className="projectPhoto">
+                    <div className={`projects ${leftActive ? 'active' : ''}`}
+                        onClick={clickHandlerLeft} style={{ backgroundImage: `url(${cattle})` }}>
+                    </div>
+                    <div className={`projects ${rightActive ? 'active' : ''}`}
+                        onClick={clickHandlerRight}>
+                    </div>
+                </div>
+            </div>
+
+            <hr />
+
+
+
+        </main>
+    )
 }
 
 export default Projects
