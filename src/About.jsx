666d592ab687e68c664e47d81b5908ee1aa0b69d
Filip <filip.ratkovic.fr@gmail.com>
2022-12-12 23:57:52 +0100

    about information

--- src/About.jsx
+++ src/About.jsx
@@ -1,59 +1,53 @@
 import React from 'react'
 import "./about.css"
-import photo from "./components/photos/photo.png";
+import linkedin from "./components/photos/linkedin.png";
 
 function About() {
   return (
     <main className="about-main">
-     <div className="text-cont">
-     <div className="about-text">
-        <h1>About me</h1>
-        <p>I'm frontend developer from Novi Pazar, Serbia.
-          I have serious passion and ambitions about developing.
-        </p>
-        <p>
-          Hardworket and well-organised person who love to learn and solve problems. 
-          Fan of sports, movies, drawing and traveling.
-           A family person and father of one daughter and son.
-        </p>
-        <p>
-          I'm  eager to learn and interested in entire frontend sprectrum, also 
-          to work with ambitious and and positive people on big projects.  
-        </p>
-      </div>
-      <div className="about-photo">
-        <img src={photo} alt="" />
-      </div>
-     </div>
+      <div className="text-cont">
+        <div className="about-text">
+          <p className="letter" ><span>A</span> <span>b</span> <span>o</span> <span>u</span> <span>t</span>
+            <span className="space"></span> <span>m</span> <span>e</span></p>
 
-      <div className="skills-cont">
-        <h1>Skills</h1>
+          <p>I'm frontend developer from Novi Pazar, Serbia.
+            I have serious passion and ambitions about developing.
+          </p>
+          <p>
+            Hardworket and well-organised person who love to learn and solve problems.
+            Fan of sports, movies, drawing and traveling.
+            A family person and father of one daughter and son.
+          </p>
+          <p>
+            I'm  eager to learn and interested in entire frontend sprectrum, also
+            to work with ambitious and and positive people on big projects.
+          </p>
+        </div>
+        <div className="about-info">
+         <div className="about-info-wrapper">
+           <div className="about-info-contact">
+            c
+          </div>
+          <div className="about-info-social">
+            <h1 className="letter"> 
+            <span>S</span>
+            <span>o</span> 
+            <span>c</span> 
+            <span>i</span>
+            <span>a</span>
+            <span>l</span></h1>
+            <div className="social-cont">
+              <img src={linkedin} alt="" />
+              <a href="https://www.linkedin.com/in/filip-ratkovic-067a96233/">www.linkedin.com/in/filip-ratkovic-067a96233/</a>
+            </div>
+          </div>
+         </div>
+          <div className="about-info-skills">
+            skill
+          </div>
+        </div>
       </div>
-      {/* <div className="about-skills">
-        <p className="letter one" ><span>H</span> <span>T</span> <span>M</span> <span>L</span></p>
-        <p className="letter six" ><span>C</span> <span>S</span> <span>S</span></p>
-        <p className="letter three" ><span>J</span> <span>A</span> <span>V</span><span>A</span> <span>S</span> <span>C</span>
-        <span>R</span> <span>I</span> <span>P</span> <span>T</span>
-        </p>
-        <p className="letter four" ><span>T</span> <span>Y</span> <span>P</span><span>E</span> <span>S</span> <span>C</span>
-        <span>R</span> <span>I</span> <span>P</span> <span>T</span>
-        </p>
-        <p className="letter five" ><span>R</span> <span>E</span> <span>A</span><span>C</span> <span>T</span> <span>.</span>
-        <span>J</span> <span>S</span>
-        </p>
-        <p className="letter two" ><span>R</span> <span>E</span> <span>A</span><span>C</span> <span>T</span> <span> </span>
-        <span>N</span> <span>A</span> <span>T</span> <span>I</span> <span>V</span> <span>E</span>
-        </p>
-        <p className="letter seven" ><span>E</span> <span>S</span> <span>5</span> <span>/</span>
-        <span>E</span> <span>S</span> <span>6</span>
-        </p>
-        <p className="letter eight" ><span>J</span> <span>S</span> <span>O</span> <span>N</span></p>
-        <p className="letter nine" ><span>G</span> <span>I</span> <span>T</span></p>
-        <p className="letter then" ><span>G</span> <span>I</span> <span>T</span>
-        <span>H</span> <span>U</span> <span>B</span>
-        </p>
-
-      </div> */}
+      
     </main>
   )
 }
