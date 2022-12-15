48d4a227a318808f22aad6879e84ac90b4e6ce12
Filip <filip.ratkovic.fr@gmail.com>
2022-12-15 22:44:46 +0100

    about side

--- src/About.jsx
+++ src/About.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import "./about.css"
-import linkedin from "./components/photos/linkedin.png";
-import github from "./components/photos/github.png";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 
 function About() {
   return (
     <main className="about-main">
+      
       <div className="text-cont">
+      <aside className="side-icons">
+
+      <FontAwesomeIcon icon={['fas', 'coffee']} />
+      </aside>
         <div className="about-text">
           <p className="letter" ><span>A</span> <span>b</span> <span>o</span> <span>u</span> <span>t</span>
             <span className="space"></span> <span>m</span> <span>e</span></p>
@@ -24,35 +29,30 @@ function About() {
             to work with ambitious and and positive people on big projects.
           </p>
         </div>
-        <div className="about-info">
-         <div className="about-info-wrapper">
-           <div className="about-info-contact">
-            c
-          </div>
-          <div className="about-info-social">
-            <h1 className="letter"> 
-            <span>S</span>
-            <span>o</span> 
-            <span>c</span> 
-            <span>i</span>
-            <span>a</span>
-            <span>l</span></h1>
-            <div className="social-cont">
-              <img src={linkedin} alt="linkedin logo" />
-              <a href="https://www.linkedin.com/in/filip-ratkovic-067a96233/">www.linkedin.com/in/filip-ratkovic-067a96233/</a>
-            </div>
-            <div className="social-cont">
-              <img src={github} alt="github logo" />
-              <a href="https://www.github.com/filip-ratkovic">github.com/filip-ratkovic</a>
-            </div>
-          </div>
-         </div>
-          <div className="about-info-skills">
-            skill
-          </div>
-        </div>
+        <aside className="side-email">
+  s
+</aside>
       </div>
-      
+<h1>sdadsa</h1>
+<h1>sdadsa</h1>
+<h1>sdadsa</h1>
+<h1>sdadsa</h1>
+<h1>sdadsa</h1>
+<h1>sdadsa</h1>
+<h1>sdadsa</h1>
+<h1>sdadsa</h1>
+<h1>sdadsa</h1>
+<h1>sdadsa</h1>
+<h1>sdadsa</h1>
+<h1>sdadsa</h1>
+<h1>sdadsa</h1>
+<h1>sdadsa</h1>
+<h1>sdadsa</h1>
+
+      {/* <div>
+<iframe src="https://fr-ecommerce.netlify.app/" frameborder="0" className="cont"></iframe>
+      </div> */}
+
     </main>
   )
 }
