0aaa5ec1c512128dc5bdb2c29597280895e7c770
Filip <filip.ratkovic.fr@gmail.com>
2022-12-27 00:36:36 +0100

    certificates

--- src/About.jsx
+++ src/About.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import "./about.css"
 import arrow from "./components/photos/arrow.png"
+import cyrcleOne from "./components/photos/cyrcleOne.jpg"
 
 
 
@@ -10,6 +11,7 @@ function About() {
 
       <div className="text-cont">
         <aside className="side-icons">
+        
           <a target="_blank" href="https://www.linkedin.com/in/filip-ratkovic-067a96233/">
             <img src="https://img.icons8.com/color/48/null/linkedin-2--v1.png" alt="linkedin logo" />
           </a>
@@ -19,15 +21,15 @@ function About() {
           </a>
           <a target="_blank" href="https://www.instagram.com/filip_ratkovic/">
             <img src="https://img.icons8.com/fluency/48/null/instagram-new.png" alt="instagram logo" />
-          </a> 
+          </a>
         </aside>
         <div className="about-text">
           <p className="letter" ><span>A</span> <span>b</span> <span>o</span> <span>u</span> <span>t</span>
             <span className="space"></span> <span>m</span> <span>e</span></p>
 
           <p>Hello! I'm frontend developer from Novi Pazar, Serbia.
-             My interest in web development started back in 2020 when i started course
-             in <span className='text-important'>Centar NIT</span>.
+            My interest in web development started back in 2020 when i started course
+            in <span className='text-important'>Centar NIT</span>.
             I have serious passion and ambitions about developing.
           </p>
           <p>
@@ -40,7 +42,7 @@ function About() {
             to work with ambitious and and positive people on big projects.
           </p>
           <p>
-          Here are a few technologies I've been lerned:
+            Here are a few technologies I've been lerned:
           </p>
           <div className="about-skill-list">
             <ul>
@@ -56,11 +58,13 @@ function About() {
         </div>
         <aside className="side-email">
           <a href="mailto:filip28r@gmail.com">
-          <p>⸻  filip28r@gmail.com   ⸻</p>
+            <p>⸻  filip28r@gmail.com   ⸻</p>
           </a>
         </aside>
+
+
       </div>
-    
+
 
       {/* <div>
 <iframe src="https://fr-ecommerce.netlify.app/" frameborder="0" className="cont"></iframe>
@@ -68,9 +72,72 @@ function About() {
 
       <div className="certificates">
       <p className="letter" ><span>C</span> <span>e</span> <span>r</span> <span>t</span> <span>i</span>
-             <span>f</span> <span>i</span> <span>c</span> <span>a</span> <span>t</span> <span>e</span> 
-             <span>s</span> </p>
+              <span>f</span> <span>i</span> <span>c</span> <span>a</span> <span>t</span> <span>e</span>
+              <span>s</span> </p>
+
+        <div className="c-info">
+          <div className="c-text">
+            
+            <h2>
+              Frontend developer
+            </h2>
+            <p className="c-title" >RISC</p>
+            <div className="c-one">
+              <p>
+                Successfully completed training: First cycle of "IT Camp - Serbia 2022" course.
+                We learn:
+              </p>
+              <div className="c-frontend-list">
+                <ul>
+                  <li> <img src={arrow} alt="arrow" />Modern JavaScript (ES6+), CSS3 and HTML5,</li>
+                  <li> <img src={arrow} alt="arrow" /> Write cross-browser compatible code,</li>
+                  <li> <img src={arrow} alt="arrow" />Apply best practices for software development,</li>
+                  <li> <img src={arrow} alt="arrow" /> Using version control system (GitHub).</li>
+                </ul>
+              </div>
+            </div>
+              <p id="c-date">Oct 2021 - May 2022</p>
+            
+           
+          </div>
+          <div className="c-photo">
+            <img src={cyrcleOne} alt="certificate" />
+          </div>
+        </div>
+
+
+        <div className="c-info">
+          <div className="c-text">
+            <h2>
+              REACT
+            </h2>
+            <p className="c-title" >RISC</p>
+            <div className="c-one">
+              <p>
+                Successfully completed training:  cycle of "IT Camp - Serbia 2022" course.
+                We learn:
+              </p>
+              <div className="c-frontend-list">
+                <ul>
+                  <li> <img src={arrow} alt="arrow" />Modern JavaScript (ES6+), CSS3 and HTML5,</li>
+                  <li> <img src={arrow} alt="arrow" /> Write cross-browser compatible code,</li>
+                  <li> <img src={arrow} alt="arrow" />Apply best practices for software development,</li>
+                  <li> <img src={arrow} alt="arrow" /> Using version control system (GitHub).</li>
+                </ul>
+              </div>
+            </div>
+            <p id="c-date">Oct 2021 - May 2022</p>
+
+           
+          </div>
+          <div className="c-photo">
+            <img src={cyrcleOne} alt="certificate" />
+          </div>
+        </div>
       </div>
+
+
+
 
     </main>
   )
